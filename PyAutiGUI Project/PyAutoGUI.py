2bc2d146f2723baa7a60a57a3019d0965fcafa64
Ayeshnoorain <121870021+Ayeshnoorain@users.noreply.github.com>
2024-03-25 15:30:30 +0100

    Create PyAutoGUI

---
+++ PyAutiGUI Project/PyAutoGUI.py
@@ -0,0 +1,17 @@
+import pyautogui
+import time
+pyautogui.PAUSE = 0.5  # Sets a 0.5 second pause after each PyAutoGUI call
+
+#pyautogui.write('Automate my tasks', interval=0.1)
+
+
+# Open the start menu
+pyautogui.press('win')
+time.sleep(1)
+# Type the name of the browser (e.g., "firefox") and press enter
+pyautogui.write('remote', interval=0.25)
+pyautogui.press('enter')
+time.sleep(2)  # Wait for the browser to open
+
+pyautogui.write('10.190.30.51', interval=0.25)
+pyautogui.press('enter')
